8b0d56f64033cb8a7e75e719a59f682438c1f516
iftahro <ifttah@gmail.com>
2026-01-20 17:13:08 +0200

    main

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -2,17 +2,34 @@ package bgu.spl.net.impl.stomp;
 
 import bgu.spl.net.api.StompMessageEncoderDecoder;
 import bgu.spl.net.api.StompMessagingProtocolImpl;
-import bgu.spl.net.srv.Reactor;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
 
     public static void main(String[] args) {
-        Server.threadPerClient(
-                3000,
-                () -> new StompMessagingProtocolImpl(),
-                () -> new StompMessageEncoderDecoder()
-        ).serve();
+        if (args.length < 2) {
+            System.out.println("Usage: StompServer <port> <tpc/reactor>");
+            return;
+        }
+        int port = Integer.parseInt(args[0]);
+        String mode = args[1];
+
+        if (mode.equals("tpc")) {
+            Server.threadPerClient(
+                    port,
+                    () -> new StompMessagingProtocolImpl(),
+                    () -> new StompMessageEncoderDecoder()
+            ).serve();
+        } else if (mode.equals("reactor")) {
+            Server.reactor(
+                    3,
+                    port,
+                    () -> new StompMessagingProtocolImpl(),
+                    () -> new StompMessageEncoderDecoder()
+            ).serve();
+        } else {
+            System.out.println("Unknown mode: " + mode + ". Use 'tpc' or 'reactor'.");
+        }
 
     }
 }
